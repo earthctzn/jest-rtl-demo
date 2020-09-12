8d0a84d421d375036758a95a81241ecb38ec8a29
earthctzn <earthctzn1@gmail.com>
2020-09-12 10:57:07 -0400

    Updates title in index.html and manifest.json, Refactors counter to remove the number from the screen when less than or equal to 1.

--- src/Counter.js
+++ src/Counter.js
@@ -3,21 +3,26 @@ import React from 'react'
 export const Counter = () => {
 
     const[count, setCount] = React.useState(0)
+    const[display, setDisplay] = React.useState(false)
 
     const handleClick = e => {
         if(e.target.id === "up"){
+            setDisplay(true)
             setCount(count + 1)
         }
         if(e.target.id === "down"){
             if(count > 0){
-               setCount(count - 1) 
+               setCount(count - 1)
+            }
+            if(count <= 1){
+                setDisplay(false)
             }
         }
     }
     return (
         <div style={{display: 'flex', flexDirection: 'column'}}>
 
-            <h1>{count}</h1>
+            {display ? <h1>{count}</h1> : null}
 
             <button 
                 id="up" 
--- src/Counter.test.js
+++ src/Counter.test.js
@@ -20,5 +20,23 @@ it("increments count by one when up is clicked.", () => {
     let num = getByText('1');
 
     // We expect our test to find the number 1 and return a truthy value.
-    expect(num).toBeTruthy();
+    expect(num).toBeInTheDocument();
+})
+
+it("decrements count by one when down is clicked.", () => {
+
+    // Destructure our query functions
+    const { getByText, getByTestId } = render(<Counter />);
+
+    // Simulate a click event on the 'up' button.
+    fireEvent.click(getByTestId('up'));
+
+    // Create the variable with the value we are expecting.
+    let num = getByText('1');
+
+    // Simulate a click event on the 'down' button.
+    fireEvent.click(getByTestId('down'));
+
+    // We expect our test to find the number 1 in the document.
+    expect(num).toBe('0');
 })
